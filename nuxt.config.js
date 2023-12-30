9432ff48820bb4da206046a0ba5534740b79a6e5
Lucas Silva <lucas.silva182@outlook.com>
2023-12-30 01:19:06 -0300

    fix: adjustment nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,11 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config, { isDev, isClient }) {
+      if (!isDev) {
+        config.output.path = "dist";
+      }
+    },
+  },
 };
